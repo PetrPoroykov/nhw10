8fb72ad2dc9d5b1af6372b727891818f2b86f8de
Petr Poroykov <pporoykov@gmail.com>
2026-05-07 21:08:48 +0300

    v1

--- src/Address.java
+++ src/Address.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Address {
+
+    private String country;
+    private String city;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Address(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return country.equals(((Address) obj).country) && city.equals(((Address) obj).city);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Map<Address, Integer> costPerAddress = new HashMap<>();
+
+        costPerAddress.put(new Address("Russia", "Moskow"), 10);
+        costPerAddress.put(new Address("Russia", "Spb"), 20);
+        costPerAddress.put(new Address("Russia", "Kazan"), 30);
+        costPerAddress.put(new Address("USA", "NY"), 100);
+
+        Scanner scanner = new Scanner(System.in);
+
+        int totalCost = 0;
+        Set<String> countriesOfDelivery = new HashSet<>();
+
+        while (true) {
+            System.out.println("""
+                    Нажмите Enter для того чтобы ввести новый заказ
+                    или наберите end и Enter для того чтобы завершить работу
+                    """);
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                break;
+            }
+            int shippingCost;
+            boolean flag = true;
+
+            System.out.println("Заполнение нового заказа.");
+            System.out.print("Введите страну: ");
+            String country = scanner.nextLine();
+            System.out.print("Введите город: ");
+            String city = scanner.nextLine();
+            Address deliveryAddress = new Address(country, city);
+            System.out.print("Введите вес (кг):  ");
+            String weightString = scanner.nextLine();
+            int weight = Integer.parseInt(weightString);
+
+            for (Map.Entry<Address, Integer> addressIntegerEntry : costPerAddress.entrySet()) {
+                if (addressIntegerEntry.getKey().equals(deliveryAddress)) {
+                    shippingCost = addressIntegerEntry.getValue() * weight;
+                    countriesOfDelivery.add(country);
+                    System.out.println("Стоимость доставки составит: " + shippingCost + " руб.");
+                    totalCost += shippingCost;
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) System.out.println("Доставки по этому адресу нет");
+            System.out.println("Общая стоимость всех доставок: " + totalCost + " руб.");
+            System.out.println("Kоличество уникальных стран были оформлены доставки " + countriesOfDelivery.size());
+
+            System.out.println(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+        }
+        System.out.println("Программа завершена!");
+    }
 }
+
