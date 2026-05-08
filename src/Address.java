4574af4a2e1f730d90c789915cbcb069d2474517
Petr Poroykov <pporoykov@gmail.com>
2026-05-08 18:42:02 +0300

    v2 без цикла в мапе

--- src/Address.java
+++ src/Address.java
@@ -3,14 +3,6 @@ public class Address {
     private String country;
     private String city;
 
-    public String getCountry() {
-        return country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
     public Address(String country, String city) {
         this.country = country;
         this.city = city;
@@ -19,5 +11,15 @@ public class Address {
     @Override
     public boolean equals(Object obj) {
         return country.equals(((Address) obj).country) && city.equals(((Address) obj).city);
+    }
+
+    @Override
+    public String toString() {
+        return country + " " + city;
+    }
+
+    @Override
+    public int hashCode() {
+        return country.hashCode() + city.hashCode();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Map<Address, Integer> costPerAddress = new HashMap<>();
 
-        costPerAddress.put(new Address("Russia", "Moskow"), 10);
-        costPerAddress.put(new Address("Russia", "Spb"), 20);
-        costPerAddress.put(new Address("Russia", "Kazan"), 30);
+        costPerAddress.put(new Address("Russia", "MSK "), 10);
+        costPerAddress.put(new Address("Russia", "SPB"), 20);
+        costPerAddress.put(new Address("Russia", "KZN"), 30);
         costPerAddress.put(new Address("USA", "NY"), 100);
+        costPerAddress.put(new Address("FRA", "PRS"), 300);
 
         Scanner scanner = new Scanner(System.in);
 
         int totalCost = 0;
+
         Set<String> countriesOfDelivery = new HashSet<>();
 
         while (true) {
@@ -23,35 +25,26 @@ public class Main {
             if ("end".equals(input)) {
                 break;
             }
-            int shippingCost;
-            boolean flag = true;
-
             System.out.println("Заполнение нового заказа.");
             System.out.print("Введите страну: ");
             String country = scanner.nextLine();
             System.out.print("Введите город: ");
             String city = scanner.nextLine();
-            Address deliveryAddress = new Address(country, city);
             System.out.print("Введите вес (кг):  ");
-            String weightString = scanner.nextLine();
-            int weight = Integer.parseInt(weightString);
-
-            for (Map.Entry<Address, Integer> addressIntegerEntry : costPerAddress.entrySet()) {
-                if (addressIntegerEntry.getKey().equals(deliveryAddress)) {
-                    shippingCost = addressIntegerEntry.getValue() * weight;
-                    countriesOfDelivery.add(country);
-                    System.out.println("Стоимость доставки составит: " + shippingCost + " руб.");
-                    totalCost += shippingCost;
-                    flag = false;
-                    break;
-                }
+            String weight = scanner.nextLine();
+            int shippingCost = costPerAddress.getOrDefault(new Address(country, city), 0) * Integer.parseInt(weight);
+            totalCost += shippingCost;
+            if (costPerAddress.containsKey(new Address(country, city))) {
+                System.out.println("Стоимость доставки составит: " + shippingCost + " руб.");
+                countriesOfDelivery.add(country);
+            } else {
+                System.out.println("Доставки по этому адресу нет");
             }
-            if (flag) System.out.println("Доставки по этому адресу нет");
             System.out.println("Общая стоимость всех доставок: " + totalCost + " руб.");
-            System.out.println("Kоличество уникальных стран были оформлены доставки " + countriesOfDelivery.size());
-
             System.out.println(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
         }
+        System.out.println("Общая стоимость всех доставок: " + totalCost + " руб.");
+        System.out.println("Kоличество уникальных стран  куда были оформлены доставки " + countriesOfDelivery.size());
         System.out.println("Программа завершена!");
     }
 }
